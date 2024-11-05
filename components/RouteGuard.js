1eebac9f93cedd4aaffd6423120c8f4763ad9bd4
Namandeep Singh Wadhwa <naman23156@gmail.com>
2024-11-04 21:30:48 -0500

    route guard added

---
+++ components/RouteGuard.js
@@ -0,0 +1,41 @@
+"use client";
+import { useRouter, usePathname } from "next/navigation";
+import { useState, useEffect } from "react";
+
+const PUBLIC_PATHS = ["/login", "/", "/_error", "/register"];
+
+export default function RouteGuard({ children }) {
+  const [isAuthenticated, setAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true); // Add a loading state
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    // Run the authentication check whenever the path changes
+    authCheck(pathname);
+  }, [pathname]);
+
+  function authCheck(url) {
+    const path = url.split("?")[0];
+
+    // Check if the token is in localStorage
+    const token = localStorage.getItem("token");
+
+    if (!token && !PUBLIC_PATHS.includes(path)) {
+      console.log(`Trying to access a protected path: ${path}`);
+      setAuthenticated(false);
+      router.push("/signin"); // Redirect if not authenticated
+    } else {
+      setAuthenticated(true); // User is authenticated or on a public path
+    }
+
+    setLoading(false); // Authentication check is complete
+  }
+
+  // Render children if authenticated or on a public path, and not loading
+  if (loading) return null; // Avoid rendering anything until loading is complete
+
+  return (
+    <>{!loading ? children : null}</>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import SideBar from "../../components/layout/sideBar";
 import SideBarMd from "../../components/layout/sideBarMD";
 import { SessionProvider } from "next-auth/react";
+import RouteGuard from "../../components/RouteGuard";
 
 
 export default function RootLayout({ children }) {
@@ -10,13 +11,15 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="min-h-screen">
         <SessionProvider>
-        <div className="sticky top-0 w-full flex justify-center bg-navbar z-50 h-14">
-          <SideBarMd />
-          <SideBar />
-        </div>
-        {/* Main content should take up remaining space */}
-        <div className=" h-svh bg-white ">{children}</div>
-        {/* Footer stays at the bottom */}
+          <RouteGuard>
+            <div className="sticky top-0 w-full flex justify-center bg-navbar z-50 h-14">
+              <SideBarMd />
+              <SideBar />
+            </div>
+            {/* Main content should take up remaining space */}
+            <div className=" h-svh bg-white ">{children}</div>
+            {/* Footer stays at the bottom */}
+          </RouteGuard>
         </SessionProvider>
       </body>
     </html>
