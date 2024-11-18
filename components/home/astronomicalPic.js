51d4af43b68a11f9ca95cbd574b5b33890441089
Namandeep Singh Wadhwa <naman23156@gmail.com>
2024-11-18 16:59:55 -0500

    astronomical image of the added

---
+++ components/home/astronomicalPic.js
@@ -0,0 +1,56 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Information( {data }) {
+  console.log(data.data)
+  useEffect(() => {
+    const elements = document.querySelectorAll(".animate-on-scroll");
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("opacity-100", "translate-y-0");
+            entry.target.classList.remove("opacity-0", "translate-y-[-100px]");
+          } else {
+            entry.target.classList.add("opacity-0", "translate-y-[-100px]");
+            entry.target.classList.remove("opacity-100", "translate-y-0");
+          }
+        });
+      },
+      { threshold: .3 }
+    );
+
+    elements.forEach((el) => observer.observe(el));
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <>
+      <div className="min-w-full"></div>
+      <div className="relative min-h-screen w-full">
+        <img
+          src={data.data.hdurl}
+          alt="NASA Image of the Day"
+          className="absolute inset-0 w-full h-full object-cover"
+        />
+      </div>
+
+      {/* Explanation Section */}
+
+      <div className="animate-on-scroll opacity-0 transform translate-y-[-100px] transition-all duration-700 ease-in-out py-10">
+        <div className="flex flex-col justify-center items-center text-2xl font-bold min-w-full">
+          <div>Astronomical image of the day</div>
+          <div className="mt-2 mb-6">
+            Copy right by :{`${data.data.copyright ?? "None"}`}
+          </div>
+        </div>
+        <h2 className="text-2xl font-bold text-center px-4">
+          {data.data.explanation}
+        </h2>
+      </div>
+    </>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,25 @@
-import Sign from "../../components/user/sign";
-export default function Home() {
-  return (
-    <>
-    <div className="h-full">
-      <Sign />
+import getImage from "../../lib/nasa/astronomicalPic";
+import Information from "../../components/home/astronomicalPic";
+
+export default async function Home() {
+  let data;
+
+  try {
+    // Fetch data on the server
+    data = await getImage();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <p className="text-xl font-bold text-red-600">Failed to load data.</p>
       </div>
-      
-    </>
+    );
+  }
+
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen ">
+      {/* Pass fetched data to the Information component */}
+      <Information data={data} />
+    </main>
   );
 }
